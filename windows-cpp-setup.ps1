db1a502715e10ba2ec0573035d0cd1de790686aa
dadler83 <113302798+dadler83@users.noreply.github.com>
2025-09-28 19:43:38 -0400

    added cmake build for raylib

--- windows-cpp-setup.ps1
+++ windows-cpp-setup.ps1
@@ -6,4 +6,10 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 choco install mingw -y;
 
 # install make
-choco install make;
+choco install make -y;
+
+# install cmake (as needed)
+choco install cmake -y;
+
+# cmake build command
+# cmake -S . -B cmake-build -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -G "MinGW Makefiles"
